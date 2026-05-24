4ced5c8c25ed56226728ab89f05ee8ffe7353a8e
Jason Liu <111265664+liug26@users.noreply.github.com>
2026-05-24 00:45:45 -0700

    Update fbag_all.sh

--- fbag_all.sh
+++ fbag_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.3
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean_eo/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.3
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_pbeo/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.3
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_unreplicated/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.3
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean/ --lower 2000 --upper 8000
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean_eo/ --lower 2000 --upper 8000
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_pbeo/ --lower 4000 --upper 10000
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_unreplicated/ --lower 4000 --upper 10000
 
 python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean/ --lower 2000 --upper 8000
 python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean_eo/ --lower 2000 --upper 8000
