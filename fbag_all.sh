d0b5aed353a46fb25d7103da036a7345453c5098
Jason Liu <111265664+liug26@users.noreply.github.com>
2026-05-24 09:08:58 -0700

    Update fbag_all.sh

--- fbag_all.sh
+++ fbag_all.sh
@@ -10,10 +10,10 @@ python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean_
 python find_boundary_and_gather.py experiment/runs/hotel_recommendations_pbeo/ --lower 4000 --upper 10000
 python find_boundary_and_gather.py experiment/runs/hotel_recommendations_unreplicated/ --lower 4000 --upper 10000
 
-python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean/ --lower 0 --upper 1000
-python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean_eo/ --lower 0 --upper 1000
-python find_boundary_and_gather.py experiment/runs/media_review_compose_pbeo/ --lower 0 --upper 2000
-python find_boundary_and_gather.py experiment/runs/media_review_compose_unreplicated/ --lower 0 --upper 2000
+python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean/ --lower 0 --upper 1000 --min-p90 0.8 --max-p90 1
+python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean_eo/ --lower 0 --upper 1000 --min-p90 0.8 --max-p90 1
+python find_boundary_and_gather.py experiment/runs/media_review_compose_pbeo/ --lower 0 --upper 2000 --min-p90 0.8 --max-p90 1
+python find_boundary_and_gather.py experiment/runs/media_review_compose_unreplicated/ --lower 0 --upper 2000 --min-p90 0.8 --max-p90 1
 
 python find_boundary_and_gather.py experiment/runs/social_compose_aegean/ --lower 2000 --upper 8000
 python find_boundary_and_gather.py experiment/runs/social_compose_aegean_eo/ --lower 2000 --upper 8000
