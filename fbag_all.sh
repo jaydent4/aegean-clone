8fbbac9498e24189f97b5cdbc9384b7d1d3d19ec
Jason Liu <111265664+liug26@users.noreply.github.com>
2026-05-24 12:31:09 -0700

    Update min and max p90 range

--- fbag_all.sh
+++ fbag_all.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean_eo/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_pbeo/ --lower 4000 --upper 10000
-python find_boundary_and_gather.py experiment/runs/hotel_hotels_unreplicated/ --lower 4000 --upper 10000
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_aegean_eo/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_pbeo/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_hotels_unreplicated/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
 
-python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean_eo/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/hotel_recommendations_pbeo/ --lower 4000 --upper 10000
-python find_boundary_and_gather.py experiment/runs/hotel_recommendations_unreplicated/ --lower 4000 --upper 10000
+python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_recommendations_aegean_eo/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_recommendations_pbeo/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/hotel_recommendations_unreplicated/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
 
-python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean/ --lower 0 --upper 1000 --min-p90 0.8 --max-p90 1
-python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean_eo/ --lower 0 --upper 1000 --min-p90 0.8 --max-p90 1
-python find_boundary_and_gather.py experiment/runs/media_review_compose_pbeo/ --lower 0 --upper 2000 --min-p90 0.8 --max-p90 1
-python find_boundary_and_gather.py experiment/runs/media_review_compose_unreplicated/ --lower 0 --upper 2000 --min-p90 0.8 --max-p90 1
+python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean/ --lower 0 --upper 1000 --min-p90 0.6 --max-p90 0.8
+python find_boundary_and_gather.py experiment/runs/media_review_compose_aegean_eo/ --lower 0 --upper 1000 --min-p90 0.6 --max-p90 0.8
+python find_boundary_and_gather.py experiment/runs/media_review_compose_pbeo/ --lower 0 --upper 2000 --min-p90 0.6 --max-p90 0.8
+python find_boundary_and_gather.py experiment/runs/media_review_compose_unreplicated/ --lower 0 --upper 2000 --min-p90 0.6 --max-p90 0.8
 
-python find_boundary_and_gather.py experiment/runs/social_compose_aegean/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/social_compose_aegean_eo/ --lower 2000 --upper 8000
-python find_boundary_and_gather.py experiment/runs/social_compose_pbeo/ --lower 4000 --upper 10000
-python find_boundary_and_gather.py experiment/runs/social_compose_unreplicated/ --lower 4000 --upper 10000
+python find_boundary_and_gather.py experiment/runs/social_compose_aegean/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/social_compose_aegean_eo/ --lower 2000 --upper 8000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/social_compose_pbeo/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
+python find_boundary_and_gather.py experiment/runs/social_compose_unreplicated/ --lower 4000 --upper 10000 --min-p90 0.2 --max-p90 0.4
